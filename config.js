0e27e9aaad2d1d9fe9b74bab95b4ae51b2e733a0
Duxlin <folorunshoabdulquodir1@gmail.com>
2022-09-27 02:48:00 +0100

    bot

---
+++ config.js
@@ -0,0 +1,57 @@
+/**
+   * Create By THE JO BOT feat AMPAZ || REACT
+   * Base by Dika Ardnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Key',
+}
+
+
+global.ownername = 'QUADHIR'
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+// Other
+global.owner = ['2347082252014','798186489257','2347046411495']
+global.premium = ['2347082252014']
+global.packname = 'Ampaz React'
+global.author = 'WhatsApp Bot'
+global.wlcm = []
+global.sessionName = 'reaction'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Group Admin Special Features!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Bot Owner Special Features',
+    group: 'Features can only be used im Group!',
+    private: 'Features can only be used in private chat!',
+    bot: 'Bot Number User Special Features',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/jo.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
